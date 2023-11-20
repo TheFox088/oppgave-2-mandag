93bdd6ba86118ccf257447cf146536c3ee612603
Sandra.B <105020067+Sandra@users.noreply.github.com>
2023-11-20 14:09:04 +0100

    oppgave 2 mandag

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function background1(){
+    document.querySelector('body').style.background = "url('image/dark-souls-armor-artorias-the-abysswalker-sif-dark-soul.png') center center/ cover";
+}
+
+function background2(){
+    document.querySelector('body').style.background = "url('image/fantasy-art-anime-snow-wolf.png') center center/ cover";
+}
